9f0c95f805c76ca75a0b3d86f6c8b4f0b521021b
Staff Asia <staffasia@Staffs-iMac.local>
2022-01-12 18:01:25 +0600

    Incomplete banner Rectangle

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Banner.scss'
 import bannerImg from '../../Images/banner-img.png';
+import { AiOutlineArrowUp } from "react-icons/ai";
 export default function Banner() {
+    
     return (
         <>
             <section className='container'>
@@ -9,10 +11,10 @@ export default function Banner() {
                     <div className='banner-content'>
                         <h2>Build Your Awesome Platform</h2>
                         <p>Enver studio is a digital studio that offers several services such as UI/UX Design to developers, we will provide the best service for those of you who use our services.</p>
-                        <button>Our Servicess</button>
+                        <button>Our Servicess <AiOutlineArrowUp className='banner-icon-ar'/></button>
                     </div>
                     <div className='banner-img'>
-                        <img src={bannerImg} alt="" />
+                        <img className='img'  src={bannerImg} alt="" />
                     </div>
                 </div>
             </section>

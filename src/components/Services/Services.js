d2bbe2154bb5aa5f19f3259c1daf05026cde0a43
Staff Asia <staffasia@Staffs-iMac.local>
2022-01-13 14:53:12 +0600

    Services Completed

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,13 +5,63 @@ export default function Services() {
     const [text, setText]= useState('');
     useEffect(() => {
            setText('The Service We Provide For You')
-    }, [text])
+    }, [text]);
+    const services = [
+        {
+            'id': '1',
+            'icon': 'fas fa-laptop-code',
+            'title': 'Development',
+            'details': 'Create a platform with the best and coolest quality from us.',
+        },
+        {
+            'id': '2',
+            'icon': 'fas fa-layer-group',
+            'title': 'UI/UX Designer',
+            'details': 'We provide UI/UX Design services, and of course with the best quality',
+        },
+        {
+            'id': '3',
+            'icon': 'fas fa-laptop-code',
+            'title': 'Graphik Designer',
+            'details': 'Create a platform with the best and coolest quality from us.',
+        },
+        {
+            'id': '4',
+            'icon': 'fas fa-map-signs',
+            'title': 'Motion Graphik',
+            'details': 'We provide Graphic Design services, with the best designers',
+        },
+    
+        {
+            'id': '5',
+            'icon': 'fas fa-camera',
+            'title': 'Photography',
+            'details': 'We provide Photography services, and of course with the best quality',
+        },
+    
+        {
+            'id': '6',
+            'icon': 'fas fa-video',
+            'title': 'Videography',
+            'details': 'Create a platform with the best and coolest quality from us.',
+        },
+    
+    ];
     return (
         <div className='container'>
             <div className='center'>
                 <Text
                     text={text}
                 ></Text>
+            </div>
+            <div className='services'>
+                {
+                    services.map(service => <div className='services-content'>
+                        <i className={service.icon}></i>
+                        <h4 className='services-title'>{service.title}</h4>
+                        <p className='p-1'>{service.details}</p>
+                    </div>
+                    )}
             </div>
         </div>
     )
